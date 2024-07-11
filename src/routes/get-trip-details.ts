51f5e25ac7e20c676757b23cdbc2d689cca34828
Lucas Dias <lucasbarbosadias@outlook.com>
2024-07-11 09:01:56 -0300

    feat: add content from third class

--- src/routes/get-trip-details.ts
+++ src/routes/get-trip-details.ts
@@ -2,10 +2,11 @@ import type { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 import { prisma } from '../lib/prisma'
+import { dayjs } from '../lib/dayjs'
 import { ClientError } from '../errors/client-error'
 
-export async function getLinks(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId/links', {
+export async function getTripDetails(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId', {
     schema: {
       params: z.object({
         tripId: z.string().uuid(), 
@@ -15,14 +16,20 @@ export async function getLinks(app: FastifyInstance) {
     const { tripId } = request.params
 
     const trip = await prisma.trip.findUnique({
+      select: {
+        id: true,
+        destination: true,
+        starts_at: true,
+        ends_at: true,
+        is_confirmed: true,
+      },
       where: { id: tripId },
-      include: { links: true}
     })
     
     if (!trip) {
       throw new ClientError('Trip not found.')
     }
 
-    return { links: trip.links }
+    return { trip }
   })
 }
